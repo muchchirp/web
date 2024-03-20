c444c24769e5ab7ffabf8fe4d85d4392e9e4401a
0xCosydell <david.awatere@gmail.com>
2024-03-20 08:57:50 +0100

    Update StripePricingTable.js

--- src/components/StripePricingTable.js
+++ src/components/StripePricingTable.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Container, Section } from "./ui";
+
 
 const PricingTable = () => {
   useEffect(() => {
@@ -17,10 +19,15 @@ const PricingTable = () => {
   }, []);
 
   return (
-    <stripe-pricing-table
-      pricing-table-id="prctbl_1Ovhy7GUjDHrefXG9FJYRmS2"
-      publishable-key="pk_live_51NAZ7SGUjDHrefXG5nHfp0EVIH1dB97WWDcpV91pulpqHNDApXhuetGWN30K1GKpDtKUHnBV64qziv1RZOuoFweU00VqnUVTei"
-    ></stripe-pricing-table>
+    <Container>
+      <Section style={{ paddingTop: '2rem', paddingBottom: '1rem' }}>
+        {/* This section now has padding applied to it */}
+        <stripe-pricing-table
+          pricing-table-id="prctbl_1OwJs8GUjDHrefXGT4ttuAcR"
+          publishable-key="pk_live_51NAZ7SGUjDHrefXG5nHfp0EVIH1dB97WWDcpV91pulpqHNDApXhuetGWN30K1GKpDtKUHnBV64qziv1RZOuoFweU00VqnUVTei"
+        ></stripe-pricing-table>
+      </Section>
+    </Container>
   );
 };
 
